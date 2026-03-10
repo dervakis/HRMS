774d603d7fd1ae1129b37c7fbb65c2339c001b91
Kishan Dervaliya <kishan.dervaliya@roimaint.com>
2026-03-10 11:00:34 +0530

    fixes issues on websocket for deployment : 2

--- hrms-api/hrms/src/main/java/com/intern/hrms/configuration/WebSocketConfiguration.java
+++ hrms-api/hrms/src/main/java/com/intern/hrms/configuration/WebSocketConfiguration.java
@@ -1,5 +1,6 @@
 package com.intern.hrms.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +10,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Value("${frontend.url}")
+    private String frontedUrl;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -19,6 +22,6 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5173").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins(frontedUrl).withSockJS();
     }
 }
